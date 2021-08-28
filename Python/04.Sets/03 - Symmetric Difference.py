72e453d04d7cfd1871f6dc0fcf97d17025c570c7
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-08-28 19:01:00 +0300

    Create 03 - Symmetric Difference.py

---
+++ Python/04.Sets/03 - Symmetric Difference.py
@@ -0,0 +1,5 @@
+# Problem: https://www.hackerrank.com/challenges/symmetric-difference/problem
+
+i, m = input(), set(map(int, input().split()))
+i, n = input(), set(map(int, input().split()))
+print(*sorted(m.symmetric_difference(n)), sep='\n')
